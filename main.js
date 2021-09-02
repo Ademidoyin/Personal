d91b3b3ee9b9fe1dee8e09926a39499be1709428
Adedoyin <duropitan@gmai.com>
2021-09-02 13:41:21 +0100

    init

---
+++ main.js
@@ -0,0 +1,17 @@
+function mine() {
+    var x = document.getElementById("history");
+if (x.style.display === "block") {
+    x.style.display = "none";
+} else {
+    x.style.display = "block"
+}
+}
+
+function goofy() {
+    var y = document.getElementById("works");
+if(y.style.display === "block") {
+    y.style.display = "none"
+} else {
+    y.style.display = "block"
+}
+}
